0203b75dec735cfec80b1410c3a073400182773c
jbwheeler1 <112184571+jbwheeler1@users.noreply.github.com>
2025-10-03 09:48:27 -0500

    Initial Commit

---
+++ CookingRecipe/CookingRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingRecipeApp.swift
+//  CookingRecipe
+//
+//  Created by Jacob Wheeler on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CookingRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CookingRecipeTests/CookingRecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CookingRecipeTests.swift
+//  CookingRecipeTests
+//
+//  Created by Jacob Wheeler on 10/3/25.
+//
+
+import Testing
+@testable import CookingRecipe
+
+struct CookingRecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
